b6efe77ef80dc45d2b3423797ea7bae502dd5e41
flpitu88 <flpitu88@hotmail.com>
2015-04-19 16:40:30 -0300

    Subo clases java implementadas con mayor funcionalidad.

--- src/main/java/model/Pasajero.java
+++ src/main/java/model/Pasajero.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Pasajero {
+	
+	private String nombre;
+	private String apellido;
+	private long dni;
+	
+	public Pasajero(String nombre, String apellido, long dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
 
 }
