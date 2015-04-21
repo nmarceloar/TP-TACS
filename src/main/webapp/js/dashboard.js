eb3a54846815ee9af3d3bd7413f73e248e4480a1
flpitu88 <flpitu88@hotmail.com>
2015-04-21 00:05:56 -0300

    Revert "agregué comentarios descriptivos"
    This reverts commit 104924eede488da26fa8cf87432659e13c4eb3bd.

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -2,13 +2,12 @@ var map;
 
 $(function(){
 	//datos de prueba
-	var cities = ['Londres','Salta','New York','R&iactue;o de Janeiro','Beirut'];
+	var cities = ['Londres','Salta','New York','Río de Janeiro','Beirut'];
 	var contViajes = 0;
 	
 	//config google maps
 	google.maps.event.addDomListener(window, 'load', initialize);
 	
-	//inicializo los forms
 	formResetViaje();
 	formResetVuelos();
 	//modal nuevo viaje ******************************
@@ -16,36 +15,26 @@ $(function(){
 	    google.maps.event.trigger(map, "resize");
 	});*/
 	
-	//cuando de hace click en un nuevo viaje
 	$("#btnNuevoViaje").click(function(event){
 		event.preventDefault();
-		//TODO debería enforcar en el primer input del form
 		$("#ciudadOrigen").focus();
-		//TODO debería arreglar el corrimiento del mapa
 		google.maps.event.trigger(map, "resize");
-		//dispara el modal de nuevo viaje
 		$("#modNuevoViaje").modal('show');
 	});
 	
-	//el autocomplete es de jqueryui
 	$("#ciudadOrigen").autocomplete({
-	  //TODO ahora carga de un array estático, debería usar una de nuestras apis que dado un string te devuelve una lista de ciudades
       source: cities,
       select: function(event, ui){
-    	  //evento de selección de elemento del autocomplete, cuando elige le habilito el campo de fecha de salida
     	  $("#fechaDesdeContainer").show();
       }
     });
 	
-	//el datepicker es un selector de fechas que te lo provee bootstrap
 	$("#fechaDesde").datepicker({
         format: 'dd/mm/yyyy',
-		//TODO falta hacer que te bloquee todas las fechas anteriores al día de hoy
-        //startDate: 'today',
+        startDate: 'today',
         todayHighlight: true
-    })
+    })//.on('changeDate', function(e){
     .change(function(e){
-    	//una vez selecciona fecha, le muestro el input de ciudad destino
     	$("#dstContainter").show();
     });
 	
@@ -75,36 +64,29 @@ $(function(){
     	      }
     	  });
       },*/
-	  //TODO acá arriba quise hacer la comunucación con despegar, pero falló. de todos modos sirve de ejemplo para usarlo con nuestra api
 	  source: cities,
       minLength: 3,
       select: function( event, ui ) {
-    	  //muetro box de fecha de vuelta
+    	  //destino
     	  $("#fechaHastaContainer").show();
       }
     });
 	
 	$("#fechaHasta").datepicker({
-		//TODO acá habría que poner que no pueda elegir fechas anteriores a la fecha de salida
         format: 'dd/mm/yyyy',
         startDate: '',
         todayHighlight: true
     })
     .change(function(e){
-    	//muestro el botón de buscar vuelo
     	$("#btnBuscarVuelo").show();
     });
 	
-	//defino acciones a realizar cuando le doy click al botón de elegir vuelos
 	$("#btnBuscarVuelo").click(function(event){
 		event.preventDefault();
-		//TODO habría que validad que las fechas sean válidads y que haya dos ciudades elegidas
-		//acá levanto los vuelos
-		getVuelos('Ida');
-		//le asigno una acción al evento de click sobre los links de vuelos que generé
-		$("a[role=vueloIda]").click(initClickIda);
-		//muestro el modal de vuelos
+		//TODO validar fechas!
 		$("#modVuelos").modal('show');
+		getVuelos('Ida');
+		$("a[role=vueloIda]").click(initClickIda);
 	});
 	
 	$("#btnCancelarViaje").click(function(event){
@@ -113,55 +95,50 @@ $(function(){
 	//**************************************************
 	
 	//modal selección de vuelos ************************
-	formResetVuelos();
+	$("#lstVuevloVuelta").hide();
+	$("#boxVueloIda").hide();
+	$("#boxVueloVuelta").hide();
 	
-	//acciones a realizar si cancelo la selección del vuelo de ida
+	/*$("a[role=vueloIda]").click(function(event){
+		event.preventDefault();
+		console.log($(this).attr("id"));
+		$("#lstVuevloVuelta").show();
+		$("#lstVuevloIda").hide();
+		$("#boxVueloIda").show();
+		getVuelos('Vuelta');
+	});*/
+	
 	$("#cancelVueloIda").click(function(event){
-		/*
-		 * básicamente lo que hace es ocultar la caja que tiene el detalle del vuelo elegido, ocultar los vuelos de vuelta y 
-		 * volver a mostrar los vuelos de ida
-		 */
 		event.preventDefault();
 		$("#boxVueloIda").hide();
 		$("#lstVuevloVuelta").hide();
 		$("#lstVuevloIda").show();
 	});
 	
-	//acciones a realizar si cancelo la selección del vuelo de vuelta
+	$("a[role=vueloVuelta]").click(function(event){
+		event.preventDefault();
+		console.log($(this).attr("id"));
+		$("#lstVuevloVuelta").hide();
+		$("#boxVueloVuelta").show();
+		$("#btnViajar").show();
+	});
+	
 	$("#cancelVueloVuelta").click(function(event){
-		//bastante similar al anterior
 		event.preventDefault();
 		$("#boxVueloVuelta").hide();
 		$("#lstVuevloVuelta").show();
 	});
 	
-	//acá es donde recién se guardaría el vuelo
 	$("#btnViajar").click(function(event){
 		event.preventDefault();
-		//el contador es solo para difernciar los datos de prueba ahora, después vuela. lo uso como id
+		$("#itemSinViaje").hide();
 		contViajes++;
-		/*
-		 * TODO acá debería llamar a la api que graba los viajes y todo lo siguiente lo haría solo si grabó bien
-		 * Si no grabó bien, debería frenar la operación e informar al usuario del error.
-		 * ****************************************************************************************************
-		 */
-		//al elemendo de la lista de viajes el adjunto el html que contiene el nuevo viaje
 		$("#listViajes").append(getViajeHTML(contViajes,$("#ciudadOrigen").val(),$("#ciudadDestino").val(),
 											 $("#fechaDesde").val(),$("#fechaHasta").val()));
-		//le asigno acciones al evento de click sobre el nuevo viaje. sería la apertura del modal de detalle
-		$("a[role=viaje][id="+contViajes+"]").click(function(event){
-			event.preventDefault();
-			//TODO levantar detalles del vuelo usando la api correspondiente
-			$("#modDetalleViaje").modal('show');
-		});
 		//limpio el form para futuros viajes
-		$("#itemSinViaje").hide();
-		//oculto el elemento inicial que dice que no hay viajes
 		formResetViaje();
 		formResetVuelos();
-		//*******************************************************************************************************
 	});
-	//si hace click en volver, solo se limpia el modal de vuelos y retorna al modal de viaje con los datos que tenía previamente
 	$("#btnVolver").click(function(e){
 		e.preventDefault();
 		$("#modNuevoViaje").modal('show');
@@ -170,12 +147,9 @@ $(function(){
 	//**************************************************
 });
 
-/*
- * dados los datos del viaje me devuelve un string con html para insertar en la lista de viajes
- */
 function getViajeHTML(idViaje, origen, destino, desde, hasta) {
-	return '<div class="list-group-item">'
-			+ '<h3 class="list-group-item-heading"><a href="#" id="'+ idViaje + '" role="viaje">Viaje 1. Desde '
+	return '<div class="list-group-item" id="'+ idViaje + '">'
+			+ '<h3 class="list-group-item-heading"><a href="#">Viaje 1. Desde '
 			+ origen
 			+ ' a '
 			+ destino
@@ -188,9 +162,6 @@ function getViajeHTML(idViaje, origen, destino, desde, hasta) {
 	+ '</div>';
 }
 
-/*
- * limpio el form inicial del viaje
- */
 function formResetViaje(){
 	$("#frmNuevoViaje")[0].reset();
 	$("#btnBuscarVuelo").hide();
@@ -201,9 +172,6 @@ function formResetViaje(){
 	$("#fechaHastaContainer").hide();
 }
 
-/*
- * limpio el form de vuelos
- */
 function formResetVuelos(){
 	$("#frmVuelos")[0].reset();
 	$("#btnViajar").hide();
@@ -224,12 +192,8 @@ function initialize() {
   };
   map=new google.maps.Map(document.getElementById("googleMapViaje"),mapProp);
   map=new google.maps.Map(document.getElementById("googleMapVuelo"),mapProp);
-  map=new google.maps.Map(document.getElementById("googleMapVueloDetalle"),mapProp);
 }
 
-/*
- * defino las acciones para lo que pasa cuando se elige el vuelo de ida
- */
 function initClickIda(){
 	$("#lstVuevloVuelta").show();
 	$("#lstVuevloIda").hide();
@@ -238,32 +202,22 @@ function initClickIda(){
 	$("a[role=vueloVuelta]").click(initClickVuelta);
 }
 
-/*
- * defino las acciones para cuando se selecciona un vuelo de vuelta
- */
 function initClickVuelta(){
 	$("#lstVuevloVuelta").hide();
 	$("#boxVueloVuelta").show();
 	$("#btnViajar").show();
 }
 
-/*
- * levanto los vuelos para la pantalla de cración de viaje
- */
 function getVuelos(sentido){
 	//TODO $.ajax a nuestra api
 	//bucle json
 	$("#sinVuelos"+sentido).hide();
 	$("#lstVuevlo"+sentido+" .list-group").html('');
-	//TODO pongo 4 solo para ver una cierta cantidad, habría que ver cuántos vuelos vamos a mostrar y si paginamos o agregamos un scroll
 	for(i=1;i<=4;i++){
 		$("#lstVuevlo"+sentido+" .list-group").append(templateVuelo(sentido,i,"aeropuerto origen","aeropuerto destino","12:45","aerolinea"));
 	}
 }
 
-/*
- * crea el snippet html que contiene los detalles del vuelo para la selección de vuelos
- */
 function templateVuelo(sentido,idVuelo,aeropuertoOrigen,aeropuertoDestion,horarioSalida,aerolinea,horarioLlegada){
 	return '<a href="#" class="list-group-item" role="vuelo'+sentido+'" id="'+idVuelo+'">'+
 				'<h4 class="list-group-item-heading">Vuelo '+idVuelo+' de '+aerolinea+' de las '+horarioSalida+' horas.</h4>'+
