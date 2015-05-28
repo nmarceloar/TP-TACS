1357b8f4f40e6669486bd7a3994f52cc1b4b21c0
mdeciervo <mdeciervo@gmail.com>
2015-05-28 12:53:02 -0300

    Fix error 500 de despegar

--- src/main/java/services/Despegar.java
+++ src/main/java/services/Despegar.java
@@ -229,7 +229,7 @@ public class Despegar
 
         } catch (InterruptedException ex) {
 
-            throw new RuntimeException("Error. Despegar 1");
+            throw new RuntimeException("Error. Despegar");
 
         } finally {
 
@@ -247,7 +247,7 @@ public class Despegar
 
             } catch (Exception ex) {
 
-                throw new RuntimeException("Error. Despegar 2");
+                throw new RuntimeException("Error. Despegar");
 
             } finally {
 
